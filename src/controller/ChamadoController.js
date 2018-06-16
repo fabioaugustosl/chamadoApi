789664c1e6901de922f2214a4fbaacc94780d34b
Fabio Lopes <fabio.lopes@gmail.com>
2018-06-16 20:12:31 -0300

    -

--- src/controller/ChamadoController.js
+++ src/controller/ChamadoController.js
@@ -755,7 +755,7 @@ var chamadoController = function(chamadoModel, grupoModel){
 		console.log(queryFinal);
 
 		chamadoModel.find(queryFinal)
-		.populate("idUnidade")
+		.populate("idUnidade").deepPopulate("idUnidade.idAgrupamento")
 		.exec(function(err, chamados){
 			if(err){
 				res.status(500).send(err);
